7b33a5ed3504d6911313501bb0ac30d874f0da0b
Matthew <mattporterihsv@gmail.com>
2020-09-30 11:27:39 -0700

    fix: comment in intent handler

--- src/intents/intentHandler.ts
+++ src/intents/intentHandler.ts
@@ -8,6 +8,8 @@ export const intentHandler = (agent: WebhookClient) => {
   const intentMap = new Map()
   intentMap.set('Default Welcome Intent', welcome);
   intentMap.set('Default Fallback Intent', fallback);
+
+  // this is a sample intent - create an intent called 'Test Webhook' in your bot to see the response
   intentMap.set('Test Webhook', testWebhook)
   return agent.handleRequest(intentMap)
 }
